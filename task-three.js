c5f45f9308632810f741a4ece18576a7686b5d7f
Diyor8229 <115524046+Diyor8229@users.noreply.github.com>
2022-11-15 21:15:43 +0500

    is else

---
+++ task-three.js
@@ -0,0 +1,71 @@
+//  Задание 5 If,  else ======================
+
+
+
+// if ("0") {
+//   console.log("Hello");
+// } else {
+//   console.log("Bye");
+// }
+
+
+//  Задание 6 ======================
+
+
+
+// let userInput = "ECMAScript";
+// if (userInput === "ECMAScript") {
+//   console.log("Right");
+// } else {
+//   console.log(`You don't know?"ECMAScript"!`);
+// }
+  
+
+
+//  Задание 7 ======================
+
+
+
+// let userInput = "5";
+// if (+userInput > 0) {
+//   console.log(1);
+// } else if (+userInput < 0) {
+//   console.log(-1);
+// } else if (+userInput === 0) {
+//   console.log(0);
+// } else {
+//   console.log("Input only number");
+// }
+
+
+
+//  Задание 8 ======================  
+
+
+
+// let result;
+// let a = 0;
+// let b = 1;
+// if (a + b < 4) {
+//   result = "Below";
+// } else {
+//   result = "Over";
+// }
+// a + b < 4 ? (result = "Below") : (result = "Over");
+// console.log(result);
+
+
+
+//  Задание 9 ======================    
+
+
+
+// let message;
+//   ? (login == "Employee")
+//   : (message = "Hello")
+//   ? login == "Director"
+//   : (message = "Greetings")
+//   ? login == ""
+//   : (message = "No login")
+//   ? (message = "");
+// console.log(message);
