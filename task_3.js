ac8c8e2558ce08d00c4ec321392cfc231ecc44d6
Diyor8229 <115524046+Diyor8229@users.noreply.github.com>
2022-11-17 23:45:33 +0500

    Switch, For Loops

---
+++ task_3.js
@@ -0,0 +1,73 @@
+//  Задание 1 ======================
+
+
+
+// let browser = "Chrome";
+// switch (browser) {
+//   case "Edge":
+//     console.log("You've got tje Edge");
+//     break;
+//   case "Chrome":
+//   case "FireFox":
+//   case "Safari":
+//   case "Opera":
+//     console.log("Okay we support these browsers too");
+//     break;
+//   default:
+//     console.log("We hope that this page looks ok!");
+// }
+// Agar browser da Edge qiymati bo'lsa faqat case "Edge" ishlidi va kod to'xtaydi,
+// Agar browser da case: Chrome, FireFox, Safari, Opera  shulardan qay biri kesaxam Okay dan boshlangan log ishlidi sababi orasida break yoq!
+// Agar xech bir case ga to'g'ri kemasa default qiymat ishlap ketadi
+
+
+
+//  Задание 2 ======================
+
+
+
+// let a = 1;
+// if (a == 0) {
+//   console.log(0);
+// }
+// if (a == 1) {
+//   console.log(1);
+// }
+// if (a == 2 || a == 3) {
+//   console.log("2,3");
+// }
+
+
+
+//  Задание 3 ======================
+
+
+
+// let num = 20;
+// for (let i = 1; i <= num; i++) {
+//   let check = 0;
+//   for (let j = 2; j < i; j++) {
+//     if (i % j == 0) {
+//       check = 1;
+//       break;
+//     }
+//   }
+//   if (i > 1 && check == 0) {
+//     console.log(i);
+//   }
+// }
+
+
+
+//  Задание 4 Таблица умножения ======================
+
+
+
+// for (let i = 2; i <= 9; i++) {
+//   console.log("===========");
+//   console.log(`${i} Karra`);
+//   console.log("===========");
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(`${i} * ${j} = ${i * j}`);
+//   }
+// }
